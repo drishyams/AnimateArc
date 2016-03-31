ac4e8700fc8437cea4ab661beb066e2c386adecb
Drishya MS <drishyams@gmail.com>
2016-03-31 15:54:47 -0700

    Updated with version that does not use keyframe

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ angular.module('AnimateArcApp', []);
 angular.module('AnimateArcApp').controller('AnimateArcController', ['$scope', function($scope)
 {
     $scope.TransferAmount = 400; // Total transfrred 
-    $scope.CashBack = 200; // CashBack
+    $scope.CashBack = 100; // CashBack
     
     $scope.ProgressClass = "semi-circle-style-0";
     $scope.CashBackClass = "Rotate-0";
@@ -13,12 +13,16 @@ angular.module('AnimateArcApp').controller('AnimateArcController', ['$scope', fu
         var cashbackString;
         
         // Calculate the percentile amount and round it it a whole number .
-        var CashPc = Math.round($scope.TransferAmount/500 * 100);
+        var DepositPercent = Math.round($scope.TransferAmount/500 * 100);
 
         var CashBackPercent = Math.round($scope.CashBack/500 * 100); //Get percentage of cashback from Total Amount
-
-        $scope.ProgressClass = "semi-circle-style-" + CashBackPercent;
-        $scope.CashBackClass = 'Rotate-' + CashPc;
+        if(DepositPercent >= CashBackPercent) {
+            $scope.ProgressClass = "semi-circle-style-" + CashBackPercent;
+            $scope.CashBackClass = 'Rotate-' + DepositPercent;
+        } else {
+            $scope.ProgressClass = "semi-circle-style-reverse-" + DepositPercent;
+            $scope.CashBackClass = 'Rotate-' + CashBackPercent;            
+        }
     };
     
 }]);
